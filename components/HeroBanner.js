4821c3291eff4cba8197ae11d12527f5e73d87b3
hicode <110273715+Hicode-dev@users.noreply.github.com>
2023-07-11 15:48:32 +0000

    this is for the cart

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -6,7 +6,7 @@ const HeroBanner = () => {
     <div>
         <div className='grid grid-cols-2 place-items-center '>
         <div className='HeroBanner_Text text-black '> 
-        <h1 className='text-black text-[3.5rem] font-bold mb-6'>Unleash Your <span className='text-[#F0A305]'>creativity</span> and Make Your Mark on the <span className='text-[#F0A305]'>World</span></h1>
+        <h1 className='text-#020F24 text-[3.5rem] font-bold mb-6'>Unleash Your <span className='text-[#F0A305]'>creativity</span> and Make Your Mark on the <span className='text-[#F0A305]'>World</span></h1>
        <p className='text-[1.4rem] font-medium mb-6'>Embark on a journey of discovery with our platform's expert guidance and immersive hands-on projects that help you harness your creative energy.</p>
        <button className='border    rounded-md bg-blue-800 text-white px-6 py-2 text-bold '>Explore Courses</button>
         </div>
