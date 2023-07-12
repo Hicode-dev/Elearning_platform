2612dfdc44c91aecb535b109d836d2cb75194bcf
codebox124 <rajcodes733@gmail.com>
2023-07-12 14:49:36 -0700

    updated

--- pages/blog.js
+++ pages/blog.js
@@ -1,10 +1,19 @@
 import Navbar from '../components/Navbar'
+import headerImg from './imgs/Background.png'
+import Image from 'next/image'
 
 export default function Blog() {
     return (
       <main>
         <Navbar />
       <div className='px-8'>
+        <div className='mt-28'>
+        <Image className = ''
+            src={headerImg}
+            alt='lop'
+
+            />
+        </div>
      
       </div>
       </main>
