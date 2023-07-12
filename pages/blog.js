afdd32a0da3e4b34b01ef58c14b29d8d687a5270
codebox124 <rajcodes733@gmail.com>
2023-07-12 11:57:19 -0700

    started blog page

---
+++ pages/blog.js
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar'
+
+export default function Blog() {
+    return (
+      <main>
+        <Navbar />
+      <div className='px-8'>
+     
+      </div>
+      </main>
+    )
+  }
